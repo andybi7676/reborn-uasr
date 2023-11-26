5259f1b02611d619612f1ac9242ec7d149b1620f
RT <yuantseng@gmail.com>
2023-11-26 15:42:56 +0800

    allow disabling of adjacent pooling

--- s2p/scripts/find_prep_align.py
+++ s2p/scripts/find_prep_align.py
@@ -1,5 +1,5 @@
 # Used for finding which raw features are pooled together after wav2vec-u preprocessing.
-# Usage: python wav2vecu_scripts/find_prep_align.py CLUS128/train.src
+# Usage: python wav2vecu_scripts/find_prep_align.py CLUS128/train.src adj
 
 # EXAMPLE
 # K-mean IDs:  24  127 115 115 79  79  65  65  65  46
@@ -8,6 +8,7 @@ import sys
 
 file = open(sys.argv[1], 'r')
 out_file = open(sys.argv[1].replace(".src", ".boundaries"), 'w')
+adj = sys.argv[2] == "adj" if len(sys.argv) > 2 else False
 
 for line in file:
     km_ids = line.strip().split()
@@ -15,24 +16,29 @@ for line in file:
     b_km = [1 if km_ids[i] != km_ids[i+1] else 0 for i in range(len(km_ids)-1)]
     b_km.append(1)
     assert len(b_km) == len(km_ids)
-    # Boundaries after adjacent pooling
-    b_adj = []
-    odd = True
-    for i in b_km:
-        if i == 1:
-            if odd:
-                b_adj.append(0)
-                odd = False
+
+    if adj:
+        # Boundaries after adjacent pooling
+        b_adj = []
+        odd = True
+        for i in b_km:
+            if i == 1:
+                if odd:
+                    b_adj.append(0)
+                    odd = False
+                else:
+                    b_adj.append(1)
+                    odd = True
             else:
-                b_adj.append(1)
-                odd = True
-        else:
-            b_adj.append(0)
-    assert len(b_adj) == len(km_ids)
-    # print(km_ids, len(km_ids))
-    # print(b_km, len(b_km), sum(b_km))
-    # print(b_adj, len(b_adj), sum(b_adj))
-    print(" ".join(str(i) for i in b_adj), file=out_file)
+                b_adj.append(0)
+        assert len(b_adj) == len(km_ids)
+        # print(km_ids, len(km_ids))
+        # print(b_km, len(b_km), sum(b_km))
+        # print(b_adj, len(b_adj), sum(b_adj))
+    
+        print(" ".join(str(i) for i in b_adj), file=out_file)
+    else:
+        print(" ".join(str(i) for i in b_km), file=out_file)
 
 file.close()
 out_file.close()
